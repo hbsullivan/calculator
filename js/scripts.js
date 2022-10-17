dacbabfa908018541c6c1454e8b970455fa3218d
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-17 15:36:08 -0700

    combined all operations into one alert
    Co-authored-by: Chloe Le <yenchi.le1199@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,21 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function division(number1, number2){
+  return number1 / number2;
+}
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The addition of your numbers equals " + add(number1, number2) + "\n" + "The subtraction of your numbers equals "  + subtract(number1, number2) + "\n" + "The multiplying of your numbers equals " + multiply(number1, number2) + "\n" + "The division of your numbers equals " + division(number1, number2) + ".");
+//window.alert("The subtraction of your numbers equals " + subtract(number1, number2));
+//window.alert("The multiplying of your numbers equals " + multiply(number1, number2));
+//window.alert("The division of your numbers equals " + division(number1, number2));
